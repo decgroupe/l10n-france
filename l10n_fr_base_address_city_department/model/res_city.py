1712cb92afefac1d7dc9b38c9971f4d2bce54d01
Yann Papouin <ypa@decgroupe.com>
2023-05-10 16:16:56 +0200

    [ADD] l10n_fr_base_address_city_department

--- l10n_fr_base_address_city_department/model/res_city.py
+++ l10n_fr_base_address_city_department/model/res_city.py
@@ -1,6 +1,6 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ResCity(models.Model):
@@ -11,3 +11,8 @@ class ResCity(models.Model):
         string="Department",
         help="Department of this city",
     )
+
+    @api.onchange("department_id")
+    def _onchange_department(self):
+        if self.department_id and not self.state_id:
+            self.state_id = self.department_id.state_id
